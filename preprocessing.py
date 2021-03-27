616bc686fed92609f35a2baac0bcf569e6cf0445
Arthur Cahu <arthur.cahu@gmail.com>
2021-03-27 18:07:20 +0100

    Fixed assignment

--- preprocessing.py
+++ preprocessing.py
@@ -1,10 +1,13 @@
 
-def preprocess(df_x):
-    df = df_x.copy()
+def preprocess(df_in):
+    df_out = df_in.copy()
+
     # "Ratio columns", i.e. features #17-19, are NaN when the denominator is 0;
-    # we invert them instead, since the numerator is never 0
+    # we invert them instead, since the numerator is never 0:
+    # a line means that there is at least one event
     ratio_cols = ['OMR', 'OCR', 'OTR']
     inverted_ratio_cols = ['I' + s for s in ratio_cols]
-    df.drop(ratio_cols)
-    df.loc[:, inverted_ratio_cols] = 1/df_x.loc[:, ratio_cols]
-    df.loc[:, inverted_ratio_cols].fillna(0, inplace=True)
+    df_out.drop(ratio_cols, axis = "columns", inplace=True)
+    df_out[inverted_ratio_cols] = (1/df_in.loc[:, ratio_cols]).fillna(0)
+
+    return df_out
